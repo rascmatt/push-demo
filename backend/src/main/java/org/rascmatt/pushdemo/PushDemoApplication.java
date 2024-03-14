f30ec2b0a1fbdedafaa08df111a8c4c9252e4b50
Matthias Raschhofer <matthias@Matthiass-MacBook-Air.local>
2024-03-14 18:34:10 +0100

    Initial commit

---
+++ backend/src/main/java/org/rascmatt/pushdemo/PushDemoApplication.java
@@ -0,0 +1,13 @@
+package org.rascmatt.pushdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushDemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/rascmatt/pushdemo/PushDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rascmatt.pushdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
